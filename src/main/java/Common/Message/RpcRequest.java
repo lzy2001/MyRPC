2f70b9825d9d68c4e52a19c26fe215b92a6ff9cd
lzy <1781486048@qq.com>
2025-05-25 14:30:06 +0800

    自定义编码器，解码器和序列化器

--- src/main/java/Common/Message/RpcRequest.java
+++ src/main/java/Common/Message/RpcRequest.java
@@ -1,11 +1,15 @@
 package Common.Message;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 public class RpcRequest implements Serializable {
